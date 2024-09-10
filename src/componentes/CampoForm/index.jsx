cb07ebfe812157589ff1bab429d500458e420b25
Filipe <fbianchi.andrade@gmail.com>
2024-09-10 15:25:37 -0300

    corrigida responsividade do botao de ocultar/mostrar senha e margin do botao de menu

--- src/componentes/CampoForm/index.jsx
+++ src/componentes/CampoForm/index.jsx
@@ -2,64 +2,55 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { FiEye, FiEyeOff } from "react-icons/fi";
 
+const Container = styled.div`
+    position: relative;
+    width: 100%;
+    display: flex;
+    align-items: center;
+`;
+
 const InputEstilizado = styled.input`
     color: #ffffff;
     font-size: 20px;
     background-color: transparent;
     border: 2px solid #6a6a6a;
     border-radius: 8px;
-    box-shadow:0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    padding: ${props => props.type === "text" || props.type === "password" ? "16px 20px" : props.type === "datetime-local" ? "16px 20px" : props.type === "number" ? "16px 20px" : '1px'};
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    padding: ${props => props.type === "text" || props.type === "password" ? "16px 60px 16px 20px" : props.type === "datetime-local" ? "16px 20px" : props.type === "number" ? "16px 20px" : '1px'};
     height: ${props => props.type === "color" ? "40px" : props.type === "checkbox" ? "30px" : "50px"};
     width: ${props => props.type === "checkbox" ? "30px" : "100%"};
-    
-    input::placeholder {
+
+    &::placeholder {
         color: #fff;
-        font-size: 10px;
+        font-size: 18px;
         opacity: 0.7;
     }
-`
+`;
 
-const IconeRevelaSenha = styled(FiEye)`
+const Icone = styled.div`
     color: #6a6a6a;
     height: 30px;
     width: 30px;
     position: absolute;
-    right: 41%;
     cursor: pointer;
+    top: 50%;
+    transform: translateY(-50%);
+    z-index: 10;
 
-    @media (max-width:1024px) {
-        right: 32%;
-    }
-    @media (max-width: 562px) {
-        right: 15%;
-    }
-`
-
-const IconeEscondeSenha = styled(FiEyeOff)`
-    color: #6a6a6a;
-    height: 30px;
-    width: 30px;
-    position: absolute;
-    right: 41%;
-    cursor: pointer;
-
-    @media (max-width:1024px) {
-        right: 32%;
-    }
-    @media (max-width: 562px) {
-        right: 11%;
-    }
-`
-
-
+    ${props => props.position === 'right' && `
+        right: 10px; /* Ajuste a posição conforme necessário */
+    `}
+    ${props => props.position === 'left' && `
+        left: 10px; /* Ajuste a posição conforme necessário */
+    `}
+`;
 
 const CampoForm = (props) => {
-    const { placeholder, type = "text", name, onChange, defaultValue } = props
+    const { placeholder, type = "text", name, onChange, defaultValue } = props;
     return (
-        <>
-            <InputEstilizado type={type} name={name} placeholder={placeholder} onChange={onChange} value={defaultValue}/>
-        </>
+        <Container>
+            <InputEstilizado type={type} name={name} placeholder={placeholder} onChange={onChange} value={defaultValue} />
+        </Container>
     );
 }
 
@@ -69,19 +60,18 @@ export const CampoSenha = (props) => {
     const { placeholder, name, onChange, defaultValue } = props;
 
     const handleClick = () => {
-        tipoDoInput === "text" ? setTipoDoInput("password") : setTipoDoInput("text");
-        
-    }
+        setTipoDoInput(prevTipo => prevTipo === "text" ? "password" : "text");
+    };
+
     return (
-        <div style={{width: '100%', display: 'flex', alignItems: 'center'}}>
+        <Container>
             <InputEstilizado type={tipoDoInput} name={name} placeholder={placeholder} onChange={onChange} value={defaultValue} />
             {
                 tipoDoInput === "password" 
-                ? <IconeRevelaSenha onClick={ () => handleClick() }/>
-                : <IconeEscondeSenha onClick={ () => handleClick() }/>
+                ? <Icone position="right" as={FiEye} onClick={handleClick} />
+                : <Icone position="right" as={FiEyeOff} onClick={handleClick} />
             }
-
-        </div>
+        </Container>
     );
 }
 
