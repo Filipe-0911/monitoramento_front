ad18df3c935d1bf032d2a980fa9d36a2e7d0ef59
Filipe <fbianchi.andrade@gmail.com>
2024-09-10 15:09:58 -0300

    corrigida exibicao do carrossel caso nao tenha nenhuma tarefa a ser exibida

--- src/Paginas/Home/index.jsx
+++ src/Paginas/Home/index.jsx
@@ -168,6 +168,7 @@ const Home = () => {
                     ?
                     <Loader />
                     :
+                    tarefas.length > 0 &&
                     <Carrossel items={
                         tarefas.map((tarefa, index) => (
                             <CardTarefas
